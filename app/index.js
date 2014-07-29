549fd7e3b3d81e5ac7a6a8e362e8601ecb074210
Michael Barreiro <MikeB_barreiro@yahoo.com>
2014-07-29 01:58:08 -0500

    home,About,albums,tours

---
+++ app/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req,res){
+  res.render('home');
+});
+
+app.get('/about', function(req,res){
+  res.render('about');
+});
+
+app.get('/disography', function(req,res){
+  res.render('disography');
+});
+
+app.get('/tour', function(req,res){
+  res.render('tour');
+});
+
+app.listen(3000, function(){
+  console.log('Express.js is listening..');
+});
